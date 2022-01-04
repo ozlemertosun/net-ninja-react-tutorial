69cfe67654706b0d95a5e1c81dd7064d4c280136
ozlemertosun <U2407193@rts-365.dk>
2022-01-04 14:12:45 +0100

    add changes

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,28 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-    return ( 
-        <div className="home">
-            <h2>Home</h2>
-        </div>
-    );
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
+
+  const handleDelete = (id) => {
+      const newBlogs = blogs.filter(blog => blog.id !== id);
+      setBlogs(newBlogs);
+  }
+
+  useEffect(() => {
+    console.log('use effect ran')
+    console.log(blogs);
+  });
+
+  return (
+    <div className="home">
+        <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
+    </div>
+  );
 }
  
 export default Home;
