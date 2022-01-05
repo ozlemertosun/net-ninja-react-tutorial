bac18d3c1fb3ad5948603ba2d20682fb1807083d
ozlemertosun <U2407193@rts-365.dk>
2022-01-05 14:17:57 +0100

    add changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import logo from './logo.svg';
+
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 
 function App() {
 
   return (
-    <div className="App"> 
+   <BrowserRouter>
+      <div className="App"> 
+        <Navbar/>
+          <div className="content">
 
-      <Navbar/>
-        <div className="content">
+      <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/create" element={<Create/>} /> 
+          <Route path="/blogs/:id" element={<BlogDetails/>} /> 
+      </Routes>
 
-          <Home/>
-      
+          </div>
       </div>
-    </div>
+    </BrowserRouter> 
   );
 }
 
